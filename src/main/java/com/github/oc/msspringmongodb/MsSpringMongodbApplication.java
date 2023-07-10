50b32b9aa30754b3eeb285c9cec9f5d3bf74222a
Ovidiu Condrache <ovidiu.condrache@gmail.com>
2023-07-10 03:29:10 +0300

    Rename Files

--- src/main/java/com/github/oc/msspringmongodb/MsSpringMongodbApplication.java
+++ src/main/java/com/github/oc/msspringmongodb/MsSpringMongodbApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringMongodbApplication {
+public class MsSpringMongodbApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringMongodbApplication.class, args);
+		SpringApplication.run(MsSpringMongodbApplication.class, args);
 	}
 }
